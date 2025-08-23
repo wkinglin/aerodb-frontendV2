a97a6c3470d22b2cf65a69b60e960b48eae17eca
wkinglin <1741858932@qq.com>
2025-08-23 22:10:46 +0800

    Refactor main Electron process and preload script for improved readability and maintainability. Update variable names for clarity, enhance window management functionality, and ensure consistent use of path module. Add comments for better understanding of the code structure.

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -1,15 +1,17 @@
-import { app as o, BrowserWindow as s, ipcMain as t } from "electron";
-import { fileURLToPath as d } from "node:url";
-import n from "node:path";
-const r = n.dirname(d(import.meta.url));
-process.env.APP_ROOT = n.join(r, "..");
-const i = process.env.VITE_DEV_SERVER_URL, w = n.join(process.env.APP_ROOT, "dist-electron"), a = n.join(process.env.APP_ROOT, "dist");
-process.env.VITE_PUBLIC = i ? n.join(process.env.APP_ROOT, "public") : a;
-let e;
-function l() {
-  e = new s({
-    icon: n.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
-    frame: !1,
+import { app, BrowserWindow, ipcMain } from "electron";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+process.env.APP_ROOT = path.join(__dirname, "..");
+const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
+const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
+const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
+let win;
+function createWindow() {
+  win = new BrowserWindow({
+    icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    frame: false,
     // 移除原生标题栏
     titleBarStyle: "hidden",
     // 隐藏标题栏
@@ -18,33 +20,55 @@ function l() {
     minWidth: 800,
     minHeight: 600,
     webPreferences: {
-      preload: n.join(r, "preload.mjs"),
-      nodeIntegration: !1,
-      contextIsolation: !0
+      preload: path.join(__dirname, "preload.mjs"),
+      nodeIntegration: false,
+      contextIsolation: true
     }
     // fullscreen: true,
-  }), e.webContents.on("did-finish-load", () => {
-    e == null || e.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
-  }), i ? e.loadURL(i) : e.loadFile(n.join(a, "index.html")), e.webContents.openDevTools();
+  });
+  win.webContents.on("did-finish-load", () => {
+    win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
+  });
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+  } else {
+    win.loadFile(path.join(RENDERER_DIST, "index.html"));
+  }
+  win.webContents.openDevTools();
 }
-o.on("window-all-closed", () => {
-  process.platform !== "darwin" && (o.quit(), e = null);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+    win = null;
+  }
 });
-o.on("activate", () => {
-  s.getAllWindows().length === 0 && l();
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
 });
-o.whenReady().then(l);
-t.on("window-min", () => {
-  e && e.minimize();
+app.whenReady().then(createWindow);
+ipcMain.on("window-min", () => {
+  if (win) {
+    win.minimize();
+  }
 });
-t.on("window-max", () => {
-  e && (e.isMaximized() ? e.unmaximize() : e.maximize());
+ipcMain.on("window-max", () => {
+  if (win) {
+    if (win.isMaximized()) {
+      win.unmaximize();
+    } else {
+      win.maximize();
+    }
+  }
 });
-t.on("window-close", () => {
-  e && e.close();
+ipcMain.on("window-close", () => {
+  if (win) {
+    win.close();
+  }
 });
 export {
-  w as MAIN_DIST,
-  a as RENDERER_DIST,
-  i as VITE_DEV_SERVER_URL
+  MAIN_DIST,
+  RENDERER_DIST,
+  VITE_DEV_SERVER_URL
 };
